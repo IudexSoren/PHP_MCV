24723dfd2ae2da59d9feb1b2d54c63d13d44be02
IudexSoren <aaronsibajam@gmail.com>
2021-05-07 14:22:07 -0600

    Ejemplo de archivo de variable de entorno agregado

---
+++ app/config/configExample.php
@@ -0,0 +1,16 @@
+<?php
+
+// Parámetros de la base de datos
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+// Raíz de la aplicación
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// Raíz de la URL (Para links dinámicos)
+define('URLROOT', 'localhost:81/mvcframework');
+
+// Nombre del sitio
+define('SITENAME', 'MVC Framework');
